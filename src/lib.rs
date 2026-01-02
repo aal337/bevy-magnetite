fab9bcd49f9763b4f7d349136152efed7a7b0baa
aal337 <86559793+aal337@users.noreply.github.com>
2026-01-02 21:09:56 +0000

    add plugin struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
+use bevy::prelude::*;
 
+#[derive(Default)]
+pub struct MagnetitePlugin;
+
+impl Plugin for MagnetitePlugin {
+    fn build(&self, _app: &mut App) {}
+}
+
+mod tests {
+    #[test]
+    fn it_works() {
+        let _ = super::MagnetitePlugin::default();
+    }
+}
