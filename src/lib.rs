9fe83d690a67f23b93e67e09d2393e15a69f6fd3
aal337 <86559793+aal337@users.noreply.github.com>
2026-01-02 21:45:03 +0000

    more tests, so I have done something useful

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,19 @@ impl Plugin for MagnetitePlugin {
     fn build(&self, _app: &mut App) {}
 }
 
+#[cfg(test)]
 mod tests {
+    use bevy::prelude::*;
+
     #[test]
     fn it_works() {
-        let _ = super::MagnetitePlugin::default();
+        let _ = super::MagnetitePlugin;
+    }
+    
+    #[test]
+    fn can_build_app() {
+        App::new()
+            .add_plugins(super::MagnetitePlugin)
+            .run();
     }
 }
